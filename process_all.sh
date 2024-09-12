ab2afd35009052ac5323120e3e022b94c0ee9bc3
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2024-09-12 22:03:00 +0200

    wip

--- process_all.sh
+++ process_all.sh
@@ -6,9 +6,12 @@ numbers=$(jq -r '.elcomNumbers[]' ./constants/elcom-numbers/elcom-numbers.json)
 # Iterate over each operator
 for number in $numbers; do
     echo "Running docker with file: $number"
-    if [ "$number" -gt "5" ]; then
-        echo "Processed desired number, exiting..."
-        exit 0
-    fi
+    # Run the docker command and check if it succeeds
     docker run -it --rm -v $(pwd):/usr/src/app -w /usr/src/app --user $(id -u):$(id -g) ghcr.io/puppeteer/puppeteer:23.2.2 node --env-file=./scraper/.env ./scraper/dist/src/single-run.js --schema-dir ./schema --output-dir ./output --database-dir ./database --elcom-numbers-json="[$number]" --prompt-file=./prompts/simple-3.txt --output-file=./output/$number/harmonized_$number.json
+
+    # If docker command fails, skip to the next number
+    if [ $? -ne 0 ]; then
+        echo "Docker command failed for number $number, skipping to the next one..."
+        continue
+    fi
 done
