ee8c798e17c75357df3144bdecb9bdee169c2e82
paploe <p.ploesser@mail.de>
2024-09-13 10:41:21 +0200

    add side menu

---
+++ demo/frontend/menu.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+def menu():
+    return
+    st.sidebar.page_link("./pages/analysis.py", label="Analysis")
+    st.sidebar.page_link("./pages/process.py", label="Process")
