3c43ce8ad58cf055d21ec1d7ff6dbb62e04a16e2
paploe <p.ploesser@mail.de>
2024-09-13 10:58:17 +0200

    add basic analysis

--- demo/frontend/menu.py
+++ demo/frontend/menu.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 
 def menu():
-    return
     st.sidebar.page_link("./pages/analysis.py", label="Analysis")
     st.sidebar.page_link("./pages/process.py", label="Process")
